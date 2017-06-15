ab839c4f09aea9badc6c5dd86adc027acb690b00
Hansung Kim <stephen422@gmail.com>
2017-06-16 01:22:59 +0900

    Add basic lookahead
    Can tell whether the current token is an identifier or not.

--- Lexer.cc
+++ Lexer.cc
@@ -1,7 +1,19 @@
 #include "Lexer.h"
+#include <cctype>
 
 void Lexer::Lex() {
   std::cout << "File size: " << src.buffer().size() << std::endl;
-  std::cout << "Dump of buffer:\n";
-  std::cout << src.buffer().data();
+  std::cout << "Current pos: " << look - std::cbegin(sv) << std::endl;
+
+	std::cout << "[" << *look << "]: ";
+	// Identifier starts with an alnum or an underscore.
+	if (std::isalnum(*look) || *look == '_') {
+		std::cout << "ident\n";
+	} else if (look == end()) {
+		std::cout << "End of file\n";
+	} else {
+		std::cout << "not ident\n";
+	}
+
+	look++;
 }
--- Lexer.h
+++ Lexer.h
@@ -3,6 +3,7 @@
 #include "Source.h"
 #include <string>
 #include <string_view>
+#include <algorithm>
 
 /// Represents a lexer state machine.
 /// This lexer assumes that the source data will outlive it.
@@ -12,9 +13,16 @@ class Lexer {
   Source &src;
   std::string_view sv;
 
+  // The lookahead pos.
+  std::string_view::iterator look;
+
+  /// Returns the end pos of the source.
+  std::string_view::iterator end() const { return std::cend(sv); }
+
 public:
   /// Make a lexer for the given file.
-  Lexer(Source &src) : src(src) {}
+  Lexer(Source &src)
+      : src(src), sv(src.buffer().data()), look(std::cbegin(sv)) {}
 
   /// Make a Lexer from the given memory buffer.  Assumes the buffer
   /// would outlive the lexer.
