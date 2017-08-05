f96fdc68fc52722dff5091c2cf24b0ce7b7bce7e
Hansung Kim <stephen422@gmail.com>
2017-08-05 22:28:38 +0900

    Prevent source read overflow by next()
    Provide a proper way to advance the lexer other than l.off++. This
    introduces a new field l.rdOff, as we read one character ahead to handle
    index out-of-bounds. The state management for look, off, and rdOff is
    entirely encapsulated in next() and New().

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
+	"unicode/utf8"
 )
+
+const EOF = -1
 
 type Lexer struct {
 	filename string
-	src []byte
-
-	// Current character offset
-	off int
+	src      []byte
+	look     rune // Lookahead character
+	off      int  // Lookahead character offset
+	rdOff    int  // File reading offset (one character after look)
 }
 
 func New(filename string) *Lexer {
@@ -20,21 +23,53 @@ func New(filename string) *Lexer {
 		panic(err)
 	}
 
-	return &Lexer{filename, src, 0}
+	l := Lexer{filename, src, 0, 0, 0}
+	l.next()
+	return &l
+}
+
+func (l *Lexer) next() {
+	if l.rdOff < len(l.src) {
+		r, w := rune(l.src[l.rdOff]), 1
+		if r >= utf8.RuneSelf {
+			r, w := utf8.DecodeRune(l.src[l.rdOff:])
+			if r == utf8.RuneError && w == 1 {
+				fmt.Fprintf(os.Stderr, "error: invalid UTF-8 character in byte %v\n", l.off)
+				os.Exit(1)
+			}
+		}
+		l.off = l.rdOff
+		l.look = r
+		l.rdOff += w
+	} else {
+		l.off = len(l.src)
+		l.look = EOF
+	}
+}
+
+func (l *Lexer) LexIdent() {
+}
+
+func isAlnum(r rune) {
 }
 
 func (l *Lexer) skipWhitespace() {
-	for l.src[l.off] == ' ' || l.src[l.off] == '\n' || l.src[l.off] == '\t' {
-		l.off++
+	for l.look == ' ' || l.look == '\n' || l.look == '\t' {
+		l.next()
 	}
 }
 
 func (l *Lexer) debug() {
-	fmt.Printf("Look: [%v] (%v/%v)\n", string(l.src[l.off]), l.off, len(l.src))
+	fmt.Print("Look: ")
+	if l.look == EOF {
+		fmt.Println("EOF")
+	} else {
+		fmt.Printf("[%v] (%v/%v)\n", string(l.look), l.off, len(l.src))
+	}
 }
 
 func main() {
-	if (len(os.Args) < 2) {
+	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: %v FILE\n", os.Args[0])
 		os.Exit(1)
 	}
