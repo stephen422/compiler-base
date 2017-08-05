abb6c52a7a4f2c0b67b3d0819a5caee27907a20f
Hansung Kim <stephen422@gmail.com>
2017-08-05 21:00:31 +0900

    Write SkipWhitespace and a debug function

--- main.go
+++ main.go
@@ -6,18 +6,42 @@ import (
 	"io/ioutil"
 )
 
-type Source []byte
+type Lexer struct {
+	filename string
+	src []byte
 
-func (s Source) print() {
-	fmt.Print(string(s))
+	// Current character offset
+	off int
 }
 
-func main() {
-	var src Source
-	src, err := ioutil.ReadFile(os.Args[1])
+func New(filename string) *Lexer {
+	src, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	src.print()
+
+	return &Lexer{filename, src, 0}
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.src[l.off] == ' ' || l.src[l.off] == '\n' || l.src[l.off] == '\t' {
+		l.off++
+	}
+}
+
+func (l *Lexer) debug() {
+	fmt.Printf("Look: [%v] (%v/%v)\n", string(l.src[l.off]), l.off, len(l.src))
+}
+
+func main() {
+	if (len(os.Args) < 2) {
+		fmt.Fprintf(os.Stderr, "usage: %v FILE\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	l := New(os.Args[1])
 	fmt.Println("Read finished!")
+	l.debug()
+	l.skipWhitespace()
+	l.debug()
 }
