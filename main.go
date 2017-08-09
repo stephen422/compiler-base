bc1d565276173e35af3b3e09fe2aa4c88262d434
Hansung Kim <stephen422@gmail.com>
2017-08-09 23:05:43 +0900

    Write identifier lexing
    Basically ripped-off version of the Go lexer. The code is so
    straightforward that I suspect it is the only right way to write an
    imperative lexer.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"unicode"
 	"unicode/utf8"
 )
 
@@ -34,7 +35,7 @@ func (l *Lexer) next() {
 		if r >= utf8.RuneSelf {
 			r, w := utf8.DecodeRune(l.src[l.rdOff:])
 			if r == utf8.RuneError && w == 1 {
-				fmt.Fprintf(os.Stderr, "error: invalid UTF-8 character in byte %v\n", l.off)
+				fmt.Fprintf(os.Stderr, "error: invalid UTF-8 character at byte %v\n", l.off)
 				os.Exit(1)
 			}
 		}
@@ -47,15 +48,36 @@ func (l *Lexer) next() {
 	}
 }
 
-func (l *Lexer) LexIdent() {
+func (l *Lexer) lexIdent() string {
+	off := l.off
+	for isLetter(l.look) || isDigit(l.look) {
+		l.next()
+	}
+	return string(l.src[off:l.off])
 }
 
-func isAlnum(r rune) {
+func isLetter(r rune) bool {
+	return 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '_' || r >= utf8.RuneSelf && unicode.IsLetter(r)
+}
+
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
 }
 
 func (l *Lexer) skipWhitespace() {
 	for l.look == ' ' || l.look == '\n' || l.look == '\t' {
 		l.next()
+	}
+}
+
+func (l *Lexer) Lex() {
+	l.skipWhitespace()
+
+	if isLetter(l.look) {
+		fmt.Printf("[%v]\n", l.lexIdent())
+	} else {
+		fmt.Fprintf(os.Stderr, "error: unrecognized token")
+		os.Exit(1)
 	}
 }
 
@@ -76,7 +98,7 @@ func main() {
 
 	l := New(os.Args[1])
 	fmt.Println("Read finished!")
-	l.debug()
-	l.skipWhitespace()
-	l.debug()
+	for l.look != EOF {
+		l.Lex()
+	}
 }
