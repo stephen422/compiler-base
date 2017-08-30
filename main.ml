874cdb8070befb22b659df871f6f18e89ecc3bc8
Hansung Kim <stephen422@gmail.com>
2017-08-31 01:06:10 +0900

    Simple lexer using sedlex

--- main.ml
+++ main.ml
@@ -1,2 +1,11 @@
+open Sedlexing
+
+let digit = [%sedlex.regexp? '0'..'9']
+
 let () =
-  print_endline "Hello, OCaml"
+  let ch = open_in_bin "main.ml" in
+  let lexbuf = Sedlexing.Utf8.from_channel ch in
+  match%sedlex lexbuf with
+  | digit -> print_endline "Hey, int!"
+  | id_start, Star id_continue -> print_endline (Sedlexing.Utf8.lexeme lexbuf)
+  | _  -> print_endline (Sedlexing.Utf8.lexeme lexbuf)
