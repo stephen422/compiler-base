f2c88e62be21e2fd5e34823e26ca88108bc822d8
Hansung Kim <stephen422@gmail.com>
2017-09-01 16:18:38 +0900

    Write basic line/col location
    via functional update of the environment record `env`. `env` currently
    only stores the current BOL infos, which is sufficient for locating
    just the current lexeme. Might need to store array of BOLs later for
    better error reporting.

--- main.ml
+++ main.ml
@@ -4,50 +4,69 @@ open Printf
 type token =
   | NUMBER
   | IDENTIFIER
+  | EOL
   | EOF
 
 type loc = (int * int)
 
 let digit = [%sedlex.regexp? '0'..'9']
 
-(** Locate the row, col position of the current lexeme. *)
-let locate lexbuf =
-  Sedlexing.loc lexbuf
+type env =
+  { bol_line : int;
+    bol_offset : int;
+  }
 
-(** Lex a token and return its (loc, token, lit). *)
-let rec lex lexbuf =
+(** Locate the row, col position of the current lexeme. *)
+let locate env lexbuf =
+  let row = env.bol_line in
+  let col = Sedlexing.lexeme_start lexbuf - env.bol_offset in
+  Printf.printf "located: %d:%d\n" row col;
+  row, col
+
+(** Lex a token and return its (env, loc, token). *)
+let rec lex env lexbuf =
   match%sedlex lexbuf with
-  (* skip whitespace *)
+  | '\n' ->
+    print_endline "EOL";
+    lex_newline env lexbuf
+
+  (* skip whitespace (other than '\n') *)
   | Plus white_space ->
-    lex lexbuf
+    lex env lexbuf
 
   | digit ->
     print_endline "NUMBER";
-    locate lexbuf, NUMBER
+    env, locate env lexbuf, NUMBER
 
   (* identifiers *)
   | id_start, Star id_continue ->
     let s, e = Sedlexing.loc lexbuf in
     Printf.printf "[%d - %d): IDENTIFIER: " s e;
     print_endline (Sedlexing.Utf8.lexeme lexbuf);
-    locate lexbuf, IDENTIFIER
+    env, locate env lexbuf, IDENTIFIER
 
   | eof ->
     print_endline "EOF";
-    locate lexbuf, EOF
+    env, locate env lexbuf, EOF
 
   | _  ->
     print_string "unknown: ";
     print_endline (Sedlexing.Utf8.lexeme lexbuf);
-    (-1, -1), EOF
+    env, (-1, -1), EOF
 
-and lex_all lexbuf =
-  let token = lex lexbuf in
+and lex_newline env lexbuf =
+  let bol_offset = Sedlexing.lexeme_start lexbuf in
+  let env = {bol_line = env.bol_line + 1;
+             bol_offset; } in
+  env, locate env lexbuf, EOL
+
+and lex_all env lexbuf =
+  let env, _, token = lex env lexbuf in
   match token with
-  | _, EOF -> ()
-  | _ -> lex_all lexbuf
+  | EOF -> ()
+  | _ -> lex_all env lexbuf
 
 let () =
   let ch = open_in_bin Sys.argv.(1) in
   let lexbuf = Sedlexing.Utf8.from_channel ch in
-  lex_all lexbuf
+  lex_all {bol_line = 1; bol_offset = 0;} lexbuf
