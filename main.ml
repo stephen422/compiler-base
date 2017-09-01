3ade5dadcf0e3ce356c7d513521acca3fd155e0b
Hansung Kim <stephen422@gmail.com>
2017-09-02 00:22:03 +0900

    Fix wrong column number calculation
    ...and write a basic pretty print function of tokens, for debugging
    purpose.
    
    Column numbers were off by one (and line numbers as well for EOLs),
    because the offset of each BOL were wrongly set as that of the _start_
    of the leading EOL, not the _end_.
    
    Reported column numbers are left to start from 0, as the OCaml
    compiler seems to do so as well.

--- main.ml
+++ main.ml
@@ -10,25 +10,30 @@ type token =
 
 type loc = (int * int) (* row, col *)
 
-let digit = [%sedlex.regexp? '0'..'9']
-
 type env =
   { bol_line : int;
     bol_offset : int;
   }
 
+let pretty_print = function
+  | NUMBER -> "NUMBER"
+  | IDENTIFIER -> "IDENTIFIER"
+  | STRING s -> "STRING: [" ^ s ^ "]"
+  | EOL -> "EOL"
+  | _ -> "unknown"
+
+let digit = [%sedlex.regexp? '0'..'9']
+
 (** Locate the row, col position of the current lexeme. *)
 let locate env lexbuf =
   let row = env.bol_line in
   let col = Sedlexing.lexeme_start lexbuf - env.bol_offset in
-  Printf.printf "located: %d:%d\n" row col;
   row, col
 
 (** Lex a token and return its (env, loc, token). *)
 let rec lex env lexbuf =
   match%sedlex lexbuf with
   | '\n' ->
-    print_endline "EOL";
     lex_newline env lexbuf
 
   (* other than \n *)
@@ -36,24 +41,18 @@ let rec lex env lexbuf =
     lex env lexbuf
 
   | digit ->
-    print_endline "NUMBER";
     env, locate env lexbuf, NUMBER
 
   (* identifiers *)
   | id_start, Star id_continue ->
-    let s, e = Sedlexing.loc lexbuf in
-    Printf.printf "[%d - %d): IDENTIFIER: " s e;
-    print_endline (Sedlexing.Utf8.lexeme lexbuf);
     env, locate env lexbuf, IDENTIFIER
 
   | '"' ->
-    print_string "STRING: ";
     let lit = Buffer.create 16 in
     let token = lex_string lit lexbuf in
     env, locate env lexbuf, token
 
   | eof ->
-    print_endline "EOF";
     env, locate env lexbuf, EOF
 
   | _  ->
@@ -62,10 +61,11 @@ let rec lex env lexbuf =
     env, (-1, -1), EOF
 
 and lex_newline env lexbuf =
-  let bol_offset = Sedlexing.lexeme_start lexbuf in
-  let env = {bol_line = env.bol_line + 1;
+  let bol_offset = Sedlexing.lexeme_end lexbuf in
+  let loc = locate env lexbuf in
+  let next_env = {bol_line = env.bol_line + 1;
              bol_offset; } in
-  env, locate env lexbuf, EOL
+  next_env, loc, EOL
 
 and lex_string lit lexbuf =
   match%sedlex lexbuf with
@@ -90,7 +90,9 @@ and lex_all env lexbuf =
   match token with
   | EOF -> ()
   | _ ->
-    print_endline "done";
+    let row, col = loc in
+    Printf.printf "%d:%d: " row col;
+    print_endline (pretty_print token);
     lex_all env lexbuf
 
 let () =
