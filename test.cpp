4b9e4a9cebff8c069d3ec2517e1e1f959ce1fab5
Hansung Kim <stephen422@gmail.com>
2018-10-12 20:00:03 +0900

    Revive lexer tests

--- test.cpp
+++ test.cpp
@@ -4,8 +4,9 @@
 
 using namespace comp;
 
-constexpr Expr *as_expr(const AstNodePtr &p) {
-    return static_cast<Expr *>(p.get());
+template <typename T>
+constexpr T *as(const AstNodePtr &p) {
+    return static_cast<T *>(p.get());
 }
 
 TEST_CASE("Expression parsing", "[parse_expr]") {
@@ -14,51 +15,55 @@ TEST_CASE("Expression parsing", "[parse_expr]") {
         Lexer l{src};
         Parser p{l};
         auto e = p.parse();
-        REQUIRE(as_expr(e)->flatten() == "((((a+b)+c)+d)+e)");
+        REQUIRE(as<Expr>(e)->flatten() == "((((a+b)+c)+d)+e)");
     }
     SECTION("operator precedence") {
         Source src{"a * b + c / d * e"};
         Lexer l{src};
         Parser p{l};
         auto e = p.parse();
-        REQUIRE(as_expr(e)->flatten() == "((a*b)+((c/d)*e))");
+        REQUIRE(as<Expr>(e)->flatten() == "((a*b)+((c/d)*e))");
+    }
+    SECTION("variable declaration") {
+        Source src{"let a;"};
+        Lexer l{src};
+        Parser p{l};
+        auto e = p.parse();
+        REQUIRE(as<VarDecl>(e)->id.lit == "a");
+        REQUIRE(as<VarDecl>(e)->mut == false);
     }
 }
 
-#if 0
 TEST_CASE("String lexing", "[lex_string]") {
-  SECTION("no escape chars") {
-    Source src{"\"Hello, there!\""};
-    Lexer lexer{src};
-    auto tok = lexer.lex_string();
-    REQUIRE(tok.s == "\"Hello, there!\"");
-  }
-  SECTION("with escape chars") {
-    // Hairy stuff...
-    Source src{"\"Hello, \\\"Bartleby!\\\"\""};
-    Lexer lexer{src};
-    auto tok = lexer.lex_string();
-    REQUIRE(tok.s == "\"Hello, \\\"Bartleby!\\\"\"");
-  }
-  SECTION("only escape chars") {
-    // Hairy stuff...
-    Source src{"\"\\\"\\n\\\"\\t\\\"\""};
-    Lexer lexer{src};
-    auto tok = lexer.lex_string();
-    REQUIRE(tok.s == "\"\\\"\\n\\\"\\t\\\"\"");
-  }
-  SECTION("meets EOS") {
-    Source src{"\"Hello,"};
-    Lexer lexer{src};
-    auto tok = lexer.lex_string();
-    REQUIRE(tok.s == "\"Hello,");
-  }
+    SECTION("no escape chars") {
+        Source src{"\"Hello, there!\""};
+        Lexer lexer{src};
+        auto tok = lexer.lex();
+        REQUIRE(tok.lit == "\"Hello, there!\"");
+    }
+    SECTION("with escape chars") {
+        Source src{"\"Hello, \\\"Bartleby!\\\"\""};
+        Lexer lexer{src};
+        auto tok = lexer.lex();
+        REQUIRE(tok.lit == "\"Hello, \\\"Bartleby!\\\"\"");
+    }
+    SECTION("only escape chars") {
+        Source src{"\"\\\"\\n\\\"\\t\\\"\""};
+        Lexer lexer{src};
+        auto tok = lexer.lex();
+        REQUIRE(tok.lit == "\"\\\"\\n\\\"\\t\\\"\"");
+    }
+    SECTION("meets EOS") {
+        Source src{"\"Hello,"};
+        Lexer lexer{src};
+        auto tok = lexer.lex();
+        REQUIRE(tok.lit == "\"Hello,");
+    }
 }
 
 TEST_CASE("Comment lexing", "[lex_comment]") {
-  Source src{"// Hello there\n// General Kenobi"};
-  Lexer lexer{src};
-  auto tok = lexer.lex_comment();
-  REQUIRE(tok.s == "// Hello there");
+    Source src{"// Hello there\n// General Kenobi"};
+    Lexer lexer{src};
+    auto tok = lexer.lex();
+    REQUIRE(tok.lit == "// Hello there");
 }
-#endif
