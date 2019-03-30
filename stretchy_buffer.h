32a749fc09bcf2fb6042a67bff2daaae54f01bd6
Hansung Kim <stephen422@gmail.com>
2019-03-30 10:42:33 +0900

    Add convenience macro for stb__sbn

--- stretchy_buffer.h
+++ stretchy_buffer.h
@@ -180,6 +180,7 @@
 #define sb_count  stb_sb_count
 #define sb_add    stb_sb_add
 #define sb_last   stb_sb_last
+#define sb_len    stb__sbn
 #endif
 
 #define stb_sb_free(a)         ((a) ? free(stb__sbraw(a)),0 : 0)
