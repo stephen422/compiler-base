6f076d3d23153eb7972eaffd44246f8c1b34b200
Hansung Kim <stephen422@gmail.com>
2021-01-28 17:26:23 +0900

    Fix test.sh to distinguish compile and runtime error

--- test.sh
+++ test.sh
@@ -4,24 +4,40 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 RS="\033[0m"
 
+fail_exit() {
+    echo "${RED}FAIL${RS} $1"
+    exit 1
+}
+
 test() {
+    local fail=0
     local ret=0
     local header=$(head -n1 $1)
-    echo ${header} | grep -q "fail" && ret=1
+    echo ${header} | grep -q "fail" && fail=1
     if echo ${header} | grep -q "exit"
     then
 	ret=$(echo ${header} | awk '{ print $3 }')
     fi
 
     rm -f ./out
-    build/ruse $1 || exit 1
-    ./out
-    if [ $? -ne $ret ]
+    build/ruse $1 2>/dev/null
+    realret=$?
+    if [ "$realret" -eq 0 ]
     then
-	echo "${RED}FAIL${RS} $1"
-	exit 1
+        ./out
+        if [ $? -ne $ret ]
+        then
+            fail_exit $1
+        else
+	    echo "${GREEN}PASS${RS} $1"
+        fi
     else
-	echo "${GREEN}PASS${RS} $1"
+        if [ "$fail" -ne 1 ]
+        then
+            fail_exit $1
+        else
+	    echo "${GREEN}PASS${RS} $1"
+        fi
     fi
 }
 
