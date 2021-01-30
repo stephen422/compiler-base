64ec1bd7d54220c27e127567225703ecc6af0942
Hansung Kim <stephen422@gmail.com>
2021-01-31 02:52:51 +0900

    check.py: make things into a function

--- check.py
+++ check.py
@@ -3,51 +3,55 @@ import re
 import sys
 import subprocess
 
-bin = 'build/ruse'
-filename = sys.argv[1]
+def test(binname, filename):
+    beacon_list = []
 
-beacon_list = []
+    line_number = 1
+    for line in open(filename, 'r'):
+        match = re.search("//~error:", line)
+        if match:
+            comment_string = match.string[match.start():]
+            # strip '//~error: ' and '\n'
+            just_beacon = comment_string[10:-1]
+            beacon_list.append((line_number, just_beacon))
+        line_number = line_number + 1
 
-line_number = 1
-for line in open(filename, 'r'):
-    match = re.search("//~error:", line)
-    if match:
-        comment_string = match.string[match.start():]
-        # strip '//~error: ' and '\n'
-        just_beacon = comment_string[10:-1]
-        beacon_list.append((line_number, just_beacon))
-    line_number = line_number + 1
+    # Run compiler and capture output
+    error_list = []
 
-# Run compiler and capture output
-error_list = []
+    r = subprocess.run([binname, filename], stdout=None, stderr=subprocess.PIPE, text=True)
+    for line in r.stderr.splitlines():
+        line_number = int(line.split(':')[1])
+        error_string = line.split(':')[4].strip()
+        error_list.append((line_number, error_string))
 
-r = subprocess.run([bin, filename], stdout=None, stderr=subprocess.PIPE, text=True)
-for line in r.stderr.splitlines():
-    line_number = int(line.split(':')[1])
-    error_string = line.split(':')[4].strip()
-    error_list.append((line_number, error_string))
-
-success = True
-i = 0
-j = 0
-while i < len(error_list) and j < len(beacon_list):
-    this_error = error_list[i]
-    this_beacon = beacon_list[j]
-    if this_error[0] == this_beacon[0]:
-        if this_error[1] != this_beacon[1]:
+    success = True
+    i = 0
+    j = 0
+    while i < len(error_list) and j < len(beacon_list):
+        this_error = error_list[i]
+        this_beacon = beacon_list[j]
+        if this_error[0] == this_beacon[0]:
+            if this_error[1] != this_beacon[1]:
+                success = False
+                print('line {}: expected: {}'.format(this_beacon[0], this_beacon[1]))
+                print('line {}: got:      {}'.format(this_error[0], this_error[1]))
+        elif this_error[0] < this_beacon[0]:
+            success = False
+            print('line {}: got:      {}'.format(this_error[0], this_error[1]))
+        else:
             success = False
             print('line {}: expected: {}'.format(this_beacon[0], this_beacon[1]))
-            print('line {}: got:      {}'.format(this_error[0], this_error[1]))
-    elif this_error[0] < this_beacon[0]:
-        success = False
-        print('line {}: got:      {}'.format(this_error[0], this_error[1]))
-    else:
-        success = False
-        print('line {}: expected: {}'.format(this_beacon[0], this_beacon[1]))
-    i = i + 1
-    j = j + 1
+        i = i + 1
+        j = j + 1
 
-if success:
-    print('\033[0;32mpass\033[0m {}'.format(filename))
-else:
-    print('\033[0;31mfail\033[0m {}'.format(filename))
+    if success:
+        print('\033[0;32mpass\033[0m {}'.format(filename))
+    else:
+        print('\033[0;31mfail\033[0m {}'.format(filename))
+
+test('build/ruse', 'test/simple.ruse')
+test('build/ruse', 'test/return.ruse')
+test('build/ruse', 'test/if.ruse')
+test('build/ruse', 'test/codegen.ruse')
+test('build/ruse', 'test/struct.ruse')
